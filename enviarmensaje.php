9a5831cc8785bb13027ac4b0c7d77df34ab2b470
rsabando <rsabando@espol.edu.ec>
2019-09-23 01:20:02 -0500

    enviar4

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -6,18 +6,16 @@ echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
-# First, instantiate the SDK with your API credentials
-$mg = Mailgun::create('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca
-');
+# Instantiate the client.
+$mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
+$domain = "samples.mailgun.org";
 
-# Now, compose and send your message.
-# $mg->messages()->send($domain, $params);
-$mg->messages()->send('sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org', [
-  'from'    => '<mailgun@sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org>',
-  'to'      => '<rsabando93@gmail.com>',
-  'subject' => 'Hello',
-  'text'    => 'Testing some Mailgun awesomness!'
-]);
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+  array('from'    => 'Excited User <excited@samples.mailgun.org>',
+        'to'      => 'Mailgun Devs <rsabando93@gmail.com>',
+        'subject' => 'Hello',
+        'text'    => 'Testing some Mailgun awesomeness!'));
 
 
 // if(mail('rsabando93@gmail.com',$correo,$mensaje )){
