022a86e921251e1c00902f79b5983d0e21df7b86
rsabando <rsabando@espol.edu.ec>
2019-09-23 01:25:31 -0500

    enviar5

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -3,19 +3,19 @@ $nombre=$_POST['Nom'];
 $correo=$_POST['Co'];
 $mensaje=$_POST['Ms'];
 echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
-$domain = "samples.mailgun.org";
-
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-  array('from'    => 'Excited User <excited@samples.mailgun.org>',
-        'to'      => 'Mailgun Devs <rsabando93@gmail.com>',
-        'subject' => 'Hello',
-        'text'    => 'Testing some Mailgun awesomeness!'));
+// require 'vendor/autoload.php';
+// use Mailgun\Mailgun;
+//
+// # Instantiate the client.
+// $mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
+// $domain = "samples.mailgun.org";
+//
+// # Make the call to the client.
+// $result = $mgClient->sendMessage("$domain",
+//   array('from'    => 'Excited User <excited@samples.mailgun.org>',
+//         'to'      => 'Mailgun Devs <rsabando93@gmail.com>',
+//         'subject' => 'Hello',
+//         'text'    => 'Testing some Mailgun awesomeness!'));
 
 
 // if(mail('rsabando93@gmail.com',$correo,$mensaje )){
