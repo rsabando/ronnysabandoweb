a092f7512c47d56fd894a69e7fb808167de3fd0e
rsabando <rsabando@espol.edu.ec>
2019-09-23 00:28:06 -0500

    enviar2

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -1,14 +1,28 @@
 <?php
-
 $nombre=$_POST['Nom'];
 $correo=$_POST['Co'];
 $mensaje=$_POST['Ms'];
 
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
 
-if(mail('rsabando93@gmail.com',$correo,$mensaje )){
-  echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
-}else{
-  echo "error"
-}
+# First, instantiate the SDK with your API credentials
+$mg = Mailgun::create('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca
+');
+
+# Now, compose and send your message.
+# $mg->messages()->send($domain, $params);
+$mg->messages()->send('ronnysabandoweb.herokuapp.com', [
+  'from'    => '<mailgun@ronnysabandoweb.herokuapp.com>',
+  'to'      => '<rsabando93@gmail.com>',
+  'subject' => 'Hello',
+  'text'    => 'Testing some Mailgun awesomness!'
+]);
+
+// if(mail('rsabando93@gmail.com',$correo,$mensaje )){
+//   echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
+// }else{
+//   echo "error"
+// }
 
  ?>
