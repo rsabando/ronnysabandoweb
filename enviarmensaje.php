d3a11273ceb92ed8b24c4c839f72266beb3ef5c5
rsabando <rsabando@espol.edu.ec>
2019-09-23 03:29:10 -0500

    enviar8

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -1,22 +1,35 @@
 <?php
-$nombre=$_POST['Nom'];
-$correo=$_POST['Co'];
-$mensaje=$_POST['Ms'];
-echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
+// $nombre=$_POST['Nom'];
+// $correo=$_POST['Co'];
+// $mensaje=$_POST['Ms'];
+// echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
 
-# Include the Autoloader (see "Libraries" for install instructions)
+// # Include the Autoloader (see "Libraries" for install instructions)
+// require 'vendor/autoload.php';
+// use Mailgun\Mailgun;
+// # Instantiate the client.
+// $mgClient = new Mailgun('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca');
+// $domain = "sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org";
+// # Make the call to the client.
+// $result = $mgClient->sendMessage($domain, array(
+// 	'from'	=> 'Excited User <app146026748@heroku.com>',
+// 	'to'	=> 'Baz <rsabando93@gmail.com>',
+// 	'subject' => 'Hello',
+// 	'text'	=> 'Testing some Mailgun awesomness!'
+// ));
+
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
 require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-# Instantiate the client.
-$mgClient = new Mailgun('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca');
-$domain = "sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org";
-# Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-	'from'	=> 'Excited User <app146026748@heroku.com>',
-	'to'	=> 'Baz <rsabando93@gmail.com>',
-	'subject' => 'Hello',
-	'text'	=> 'Testing some Mailgun awesomness!'
-));
+$sendgrid = new SendGrid("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");
+$email    = new SendGrid\Email();
+
+$email->addTo("test@sendgrid.com")
+      ->setFrom("rsabando93@gmail.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
 
 
 // if(mail('rsabando93@gmail.com',$correo,$mensaje )){
