a42f8cf5ffb3905b9a611c3eb345e6b9fe6933a2
rsabando <rsabando@espol.edu.ec>
2019-09-23 02:05:28 -0500

    enviar7

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -12,7 +12,7 @@ $mgClient = new Mailgun('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca');
 $domain = "sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org";
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
-	'from'	=> 'Excited User <mailgun@sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org>',
+	'from'	=> 'Excited User <app146026748@heroku.com>',
 	'to'	=> 'Baz <rsabando93@gmail.com>',
 	'subject' => 'Hello',
 	'text'	=> 'Testing some Mailgun awesomness!'
