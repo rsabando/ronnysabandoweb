afbce386c85aafaac61b11dd6340c8bdbedbb244
rsabando <rsabando@espol.edu.ec>
2019-09-23 01:08:31 -0500

    enviar3

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -2,7 +2,7 @@
 $nombre=$_POST['Nom'];
 $correo=$_POST['Co'];
 $mensaje=$_POST['Ms'];
-
+echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
@@ -12,12 +12,13 @@ $mg = Mailgun::create('15d9014cf6105329170531ecb60d67d9-baa55c84-f51cd5ca
 
 # Now, compose and send your message.
 # $mg->messages()->send($domain, $params);
-$mg->messages()->send('ronnysabandoweb.herokuapp.com', [
-  'from'    => '<mailgun@ronnysabandoweb.herokuapp.com>',
+$mg->messages()->send('sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org', [
+  'from'    => '<mailgun@sandbox4ec4c77889c0425fafd2b43ff87dff61.mailgun.org>',
   'to'      => '<rsabando93@gmail.com>',
   'subject' => 'Hello',
   'text'    => 'Testing some Mailgun awesomness!'
 ]);
+
 
 // if(mail('rsabando93@gmail.com',$correo,$mensaje )){
 //   echo "Hola ".$nombre." con correo ".$correo." tu mensaje es: ". $mensaje;
